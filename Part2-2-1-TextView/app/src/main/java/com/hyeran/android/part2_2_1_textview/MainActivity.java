341cd1a302d842d77077207124e89c2f0e36d044
HyeranShin <hyebaragi__@naver.com>
2018-09-14 13:27:18 +0900

    9/14

--- Part2-2-1-TextView/app/src/main/java/com/hyeran/android/part2_2_1_textview/MainActivity.java
+++ Part2-2-1-TextView/app/src/main/java/com/hyeran/android/part2_2_1_textview/MainActivity.java
@@ -2,12 +2,16 @@ package com.hyeran.android.part2_2_1_textview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // setContentView(R.layout.activity_main);
+        TextView obj1 = new TextView(this);
+        obj1.setText("Welcome! 2018 모바일 프로그래밍 (Java Code)");
+        setContentView(obj1);
     }
 }
