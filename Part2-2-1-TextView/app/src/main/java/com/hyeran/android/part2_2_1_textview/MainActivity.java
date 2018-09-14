9fefeb82b17445ccd891c7dcff21e140e14638e8
HyeranShin <hyebaragi__@naver.com>
2018-09-14 10:18:29 +0900

    first commit

---
+++ Part2-2-1-TextView/app/src/main/java/com/hyeran/android/part2_2_1_textview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hyeran.android.part2_2_1_textview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
