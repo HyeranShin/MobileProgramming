21aeca75986af63ae19de03f6eca307dbe931178
HyeranShin <hyebaragi__@naver.com>
2018-09-28 11:54:05 +0900

    9/28 Widget

---
+++ Part3-3-2-Button/app/src/main/java/com/hyeran/android/part3_3_2_button/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hyeran.android.part3_3_2_button;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
